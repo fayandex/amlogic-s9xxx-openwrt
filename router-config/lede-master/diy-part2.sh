1dbdad70161112560d5c916160e2d9fe6433f945
facerleo <86334632+facerleo@users.noreply.github.com>
2022-09-21 18:45:31 -0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -4,10 +4,10 @@
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 
 # Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.0.31/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /faceroo compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings  
